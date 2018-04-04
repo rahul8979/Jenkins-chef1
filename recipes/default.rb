d01eaf747d98cf3b548bf048d3cd7d87ac4b3412
rahul8979 <rahul2@tothenew.com>
2018-04-04 10:48:54 +0530

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package "nginx" do
+ action :install
+end
+
+service "nginx" do
+  action :start
+end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -13,6 +13,13 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe package('nginx') do 
+ it { should be_installed }
+end
+describe service('nginx') do 
+  it { should be_enabled }
 end
